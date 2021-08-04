21e844547ee8cda48c36930cc2f30f30d90c7250
Glitch (glitch-hello-react) <none>
2021-08-04 08:27:56 +0000

    🏭🙏 Checkpoint ./src/styles/styles.css:15782344/921 ./src/components/navigation.jsx:15782344/3236

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 
-
 export default () => (
-    <div classname="navbar">
+  <div
+    style={{
+      position: "fixed",
+      bottom: 0,
+      width: "100vw",
+        height: '64px',
+      borderTop: "2px solid #f9f9f9"
+    }}
+  >
+    <div
+      style={{
+        width: "100%",
+        height: '100%',
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        alignItems: "center"
+      }}
+    >
       <div>家</div>
       <div>女の子</div>
-      <div>女の子</div>
+      <div>Settings</div>
     </div>
+  </div>
 );
