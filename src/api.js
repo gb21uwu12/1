7b80fff92c7acad82ccf1d1b16566a22673c1d3c
Glitch (glitch-hello-react) <none>
2021-08-04 14:10:15 +0000

    🌍🚎 Checkpoint ./src/api.js:15782344/19

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export const posts =
-  "https://public-api.wordpress.com/rest/v1.2/sites/niggastash.wordpress.com/posts";
+  "https://public-api.wordpress.com/rest/v1.2/sites/gb21uwu12.wordpress.com/posts";
 
 export const girls = "";
 
