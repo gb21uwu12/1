19ac9af78ac0bee81607b4b284c209b69da1db12
Glitch (glitch-hello-react) <none>
2021-08-04 09:35:23 +0000

    🌹🌝 Checkpoint ./src/api.js:15782344/461 ./src/pages/home.jsx:15782344/7849 ./src/pages/idol.jsx:15782344/737 ./src/pages/tag.jsx:15782344/764

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,38 @@
 import * as React from "react";
+import { posts } from "../api";
 /* ADD IMPORTS FROM TODO ON THE NEXT LINE */
 
-
 /**
-* The About function defines the component that makes up the About page
-* This component is attached to the /about path in router.jsx
-*/
+ * The About function defines the component that makes up the About page
+ * This component is attached to the /about path in router.jsx
+ */
 
 export default function Home() {
   /* DECLARE STYLE AND TRIGGER FOR WIGGLE EFFECT FROM TODO ON NEXT LINE */
+  const [post, setPost] = React.useState([]);
+
   React.useEffect(() => {
-     fetch('https://api.mydomain.com')
+    fetch(posts)
       .then(response => response.json())
-      .then(data => console.log(data));
-  },[])
-  
+      .then(data => setPost(data.posts));
+  }, []);
+
   return (
     <>
-      Home
+      <h1>はじめに</h1>
+      {post.map(item => (
+        <Card item={item} key={item.ID} />
+      ))}
     </>
   );
 }
+
+const Card = ({ item }) => {
+  console.log(item);
+  return (
+    <div>
+      <p>{item.title}</p>
+      <p>{item.modified}</p>
+    </div>
+  );
+};
